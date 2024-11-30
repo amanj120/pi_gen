ba10e78f4000571173f15567e002471421fbec95
Aman Jain <aman@mac.lan>
2024-11-30 09:09:59 -0500

    simplify code

--- pi3.cpp
+++ pi3.cpp
@@ -1,80 +1,13 @@
 #include <iostream>
-#include <sstream>
 #include <vector>
-#include <utility>
 #include <cassert>
 
-using std::vector;
-using std::pair;
-using std::stringstream;
-
-using Int     = uint64_t;
-using IntPair = pair<Int, Int>;
-using BigInt  = vector<Int>;
-
-constexpr uint64_t half_64 = 0xffffffff;
-constexpr uint32_t half_32 = 0xffff;
-constexpr uint16_t half_16 = 0xff;
-constexpr uint8_t  half_8  = 0xf;
-
-// least significant bits
-uint64_t rh(const uint64_t x) {
-    return x & half_64;
-}
-// most significant bits
-uint64_t lh(const uint64_t x) {
-    return (x >> 32) & half_64;
-}
-
-// least significant bits
-uint32_t rh(const uint32_t x) {
-    return x & half_32;
-}
-// most significant bits
-uint32_t lh(const uint32_t x) {
-    return (x >> 16) & half_32;
-}
-
-// least significant bits
-uint16_t rh(const uint16_t x) {
-    return x & half_16;
-}
-// most significant bits
-uint16_t lh(const uint16_t x) {
-    return (x >> 8) & half_16;
-}
-
-// least significant bits
-uint8_t rh(const uint8_t x) {
-    return x & half_16;
-}
-// most significant bits
-uint8_t lh(const uint8_t x) {
-    return (x >> 4) & half_8;
-}
-
-// Returns in little-endian order
-IntPair mul(const Int x, const Int y) {
-    auto const xl = lh(x);
-    auto const xr = rh(x);
-    auto const yl = lh(y);
-    auto const yr = rh(y);
-    auto const ll = xl * yl;
-    auto const lr = xl * yr;
-    auto const rl = xr * yl;
-    auto const rr = xr * yr;
-    auto const o0 = rh(rr);
-    auto const o1 = rh(lr) + rh(rl) + lh(rr);
-    auto const o2 = rh(ll) + lh(lr) + lh(rl) + lh(o1);
-    auto const o3 = lh(ll);
-    auto const pr = (o3 << 32) + o2;
-    auto const pl = (o1 << 32) + o0;
-
-    return {pl, pr};
-}
+using Int    = uint64_t;
+using BigInt = std::vector<Int>;
 
 // Returns false if b > a, else does a -= b in place and returns true.
 bool diffeq(BigInt& a, const BigInt& b) {
+    // checking to make sure a >= b;
     if (b.size() > a.size()) {
         return false;
     }
@@ -85,7 +18,7 @@ bool diffeq(BigInt& a, const BigInt& b) {
             return false;
         }
     }
-    Int carry = 0;
+
     for (size_t i = 0; i < b.size(); i++) {
         if (b[i] > a[i]) {
             // TODO: deal with multiple 0's in a row
@@ -101,48 +34,43 @@ bool diffeq(BigInt& a, const BigInt& b) {
 }
 
 // s = a * x + y
-void saxpy(BigInt& s, const BigInt& a, const Int& x, const BigInt& y) {
-    // mul
+void saxpy(BigInt& s, const BigInt& a, const Int x, const BigInt& y) {
+    constexpr Int half_64 = 0xffffffff;
+
     Int p_carry = 0; // carry for product
     Int s_carry = 0; // carry for sum
 
-    if (a.size() >= y.size()) {
-        s.resize(a.size() + 1, 0UL);
+    const Int ys = y.size();
+    const Int as = a.size();
+    if (as >= ys) {
+        s.resize(as + 1, 0UL);
     } else {
-        s.resize(y.size() + 1, 0UL);
+        s.resize(ys + 1, 0UL);
     }
 
-    Int s_idx; 
-    for (size_t idx = 0; idx < s.size(); idx++) {
-        s_idx = 0;
-        
-        if (idx < a.size()) {
-            auto const [pl, pr] = mul(a[idx], x);
-            s_idx = p_carry + pl;
-            p_carry = pr;
-            if (s_idx < pl) {
-                p_carry += 1;
-            } 
-        } else if (idx == a.size()) {
-            s_idx = p_carry;
-        }
+    for (size_t i = 0; i < s.size(); i++) {
+        const Int ai = i < as ? a[i] : 0UL;
+        const Int yi = i < ys ? y[i] : 0UL;
 
-        if (idx < y.size()) {
-            const Int old_si = s_idx;
-            s_idx += y[idx] + s_carry;
-            if (s_idx > old_si) {
-                s_carry = 0;
-            } else if (s_idx < old_si) {
-                s_carry = 1;
-            } // else if s_idx == old_si then carry stays the same
-        } else if (s_carry > 0) {
-            s_idx += s_carry;
-            if (s_idx != 0 && s_carry == 1) { // no overflow
-                s_carry = 0;
-            }
-        }
-        
-        s[idx] = s_idx;
+        const Int xm = x >> 32;         // most significant half of x
+        const Int xl = x & half_64;     // least significant half of x
+        const Int am = ai >> 32;        // most significant half of a[i]
+        const Int al = ai & half_64;    // least significant half of a[i]
+        const Int mm = xm * am;
+        const Int ml = xm * al;
+        const Int lm = xl * am;
+        const Int ll = xl * al;
+        const Int o1 = (ml & half_64) + (lm & half_64) + (ll >> 32);
+        const Int pm = mm + (ml >> 32) + (lm >> 32) + (o1 >> 32); // most significant half of product
+        const Int pl = (o1 << 32) + (ll & half_64); // least significant half of product
+
+        const Int sip = p_carry + pl;       // s[i] after product (to calculate p_carry)
+        const Int sis = sip + yi + s_carry; // s[i] after sum (to calculate s_carry)
+
+        p_carry = pm + ((sip < pl) * 1);
+        s_carry = ((sis < sip) * 1) + ((sis == sip) * s_carry);
+
+        s[i] = sis;
     }
 
     while (s.back() == 0 && s.size() > 1) {
@@ -153,50 +81,45 @@ void saxpy(BigInt& s, const BigInt& a, const Int& x, const BigInt& y) {
 int main(int argc, char* argv[]) {
     assert(argc == 2);
     Int N = std::stoull(argv[1]);
-    // Any bigger and x3 gets close to overflowing.
+    // Any bigger and x1 gets close to overflowing.
     assert(N <= 369'000'000UL);
-
-    stringstream pi;
 
     BigInt n0 = {1};
     BigInt n1 = {0};
     BigInt n2 = {1};
 
-    const BigInt zero = {0};
+    const BigInt zero = {};
 
     Int x0 = 5;
     Int x1 = 300;
     Int x2 = 30;
     Int x3 = 3;
-    Int i  = 1;
 
-    while (N >= i) {
-        if (i % 1000 == 0) {
-            std::cerr << i << std::endl;
+    for (Int i = 1; i <= N; ++i) {
+        if (i % 128 == 0) {
+            std::cout << std::endl << std::flush;
         }
         
-        // n1 = (n0 * x3) + n1
-        saxpy(n1, n0, x3, n1);
+        
+        saxpy(n1, n0, x3, n1);      // n1 = (n0 * x3) + n1 
 
         char y = '0';
         while (diffeq(n1, n2)) {
             y += 1;
         }
         assert(y <= '9');
-        pi << y;
+        std::cout << y;
 
         saxpy(n0, n0, x0, zero);    // n0 *= x0
         saxpy(n1, n1, x1, zero);    // n1 *= x1
         saxpy(n2, n2, x2, zero);    // n2 *= x2
 
-        i  += 1;
-        x0 += 20 * i;
-        x0 -= 15;
-        x1 += 270 * i;
-        x2 += 27 * i;
+        // i  += 1;
+        x0 += (20 * i) + 5;
+        x1 += 270 * (i + 1);
+        x2 += 27 * (i + 1);
         x3 += 5;
     }
-
-    std::cout << pi.str() << std::endl;
+    std::cout << std::endl << std::flush;
 }
 
