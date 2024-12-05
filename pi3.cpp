ea0573b4160b3efc28cad7fcfd5e335499e0bef8
Aman Jain <aman@mac.lan>
2024-12-05 00:45:07 -0500

    consolidate into saxpy

--- pi3.cpp
+++ pi3.cpp
@@ -1,105 +1,82 @@
 #include <iostream>
 
-// Returns false if b > a, else does a -= b in place and returns true
-bool diffeq(uint64_t* a, uint64_t& asize, uint64_t* b, uint64_t& bsize) {
-    // checking to make sure a >= b;
-    if (bsize > asize) {
-        return false;
-    }
-    for (auto i = asize; i > 0; --i) {
-        if (a[i - 1] > b[i - 1]) {
-            break;
-        } else if (b[i - 1] > a[i - 1]) {
-            return false;
-        }
-    }
+void mul(const uint64_t x, const uint64_t y, uint64_t& pm, uint64_t& pl) {
+    constexpr uint64_t shift = (sizeof(uint64_t) * 4UL);
+    constexpr uint64_t half  = static_cast<uint64_t>(0xffffffffffffffffUL >> shift);
 
-    for (size_t i = 0; i < bsize; i++) {
-        if (b[i] > a[i]) {
-            // TODO: deal with multiple 0's in a row
-            // assert(a[i + 1] != 0);
-            a[i + 1] -= 1;
-        }
-        a[i] -= b[i];
-    }
-    while (a[asize - 1] == 0 && asize > 0) {
-        asize--;
-    }
-    return true;
+    const uint64_t xm = x >> shift;  // most significant half of x
+    const uint64_t xl = x & half;    // least significant half of x
+    const uint64_t ym = y >> shift;  // most significant half of y
+    const uint64_t yl = y & half;    // least significant half of y
+    const uint64_t mm = xm * ym;
+    const uint64_t ml = xm * yl;
+    const uint64_t lm = xl * ym;
+    const uint64_t ll = xl * yl;
+    const uint64_t o1 = (ml & half) + (lm & half) + (ll >> shift);
+    pm = mm + (ml >> shift) + (lm >> shift) + (o1 >> shift); // most significant half of product
+    pl = (o1 << shift) + (ll & half); // least significant half of product
 }
 
 // s = a * x + y
 void saxpy(uint64_t* s, uint64_t& ssize, uint64_t* a, uint64_t& asize, const uint64_t x, uint64_t* y, uint64_t& ysize) {
-    constexpr size_t shift = (sizeof(uint64_t) * 4UL);
-    constexpr uint64_t    half  = static_cast<uint64_t>(0xffffffffffffffffUL >> shift);
+    if (y != nullptr) {
+        uint64_t p_carry = 0; // carry for product
+        uint64_t s_carry = 0; // carry for sum
+        uint64_t pm; // most significant half of 64 bit multiply.
+        uint64_t pl; // least significant half of 64 bit multiply.
 
-    uint64_t p_carry = 0; // carry for product
-    uint64_t s_carry = 0; // carry for sum
-    if (asize >= ysize) {
-        ssize = asize + 1;
+        if (asize >= ysize) {
+            ssize = asize + 1;
+        } else {
+            ssize = ysize + 1;
+        }
+
+        for (size_t i = 0; i < ssize; i++) {
+            mul(a[i], x, pm, pl);
+            const uint64_t sip = p_carry + pl;  // s[i] after product (to calculate p_carry)
+            s[i] = sip + y[i] + s_carry;        // s[i] after sum (to calculate s_carry)
+            p_carry = pm + ((sip < pl) * 1);
+            s_carry = ((s[i] < sip) * 1) + ((s[i] == sip) * s_carry);
+        }
+
+        while (s[ssize - 1] == 0 && ssize > 0) {
+            ssize--;
+        }
     } else {
-        ssize = ysize + 1;
+        uint64_t p_carry = 0; // carry for product
+        uint64_t pm; // most significant half of 64 bit multiply.
+        uint64_t pl; // least significant half of 64 bit multiply.
+
+        ssize = asize + 1;
+        for (size_t i = 0; i < ssize; i++) {
+            mul(a[i], x, pm, pl);
+            s[i] = p_carry + pl;
+            p_carry = pm + ((s[i] < pl) * 1);
+        }
+
+        while (s[ssize - 1] == 0 && ssize > 0) {
+            ssize--;
+        }
     }
+    
+}
 
+// s = a * x
+void sax(uint64_t*s, uint64_t& ssize, uint64_t* a, uint64_t& asize, const uint64_t x) {
+    uint64_t p_carry = 0; // carry for product
+    uint64_t pm; // most significant half of 64 bit multiply.
+    uint64_t pl; // least significant half of 64 bit multiply.
+
+    ssize = asize + 1;
     for (size_t i = 0; i < ssize; i++) {
-        const uint64_t ai = a[i];
-        const uint64_t yi = y[i];
-        const uint64_t xm = x >> shift;   // most significant half of x
-        const uint64_t xl = x & half;     // least significant half of x
-        const uint64_t am = ai >> shift;  // most significant half of a[i]
-        const uint64_t al = ai & half;    // least significant half of a[i]
-        const uint64_t mm = xm * am;
-        const uint64_t ml = xm * al;
-        const uint64_t lm = xl * am;
-        const uint64_t ll = xl * al;
-        const uint64_t o1 = (ml & half) + (lm & half) + (ll >> shift);
-        const uint64_t pm = mm + (ml >> shift) + (lm >> shift) + (o1 >> shift); // most significant half of product
-        const uint64_t pl = (o1 << shift) + (ll & half); // least significant half of product
-
-        const uint64_t sip = p_carry + pl;       // s[i] after product (to calculate p_carry)
-        const uint64_t sis = sip + yi + s_carry; // s[i] after sum (to calculate s_carry)
-
-        p_carry = pm + ((sip < pl) * 1);
-        s_carry = ((sis < sip) * 1) + ((sis == sip) * s_carry);
-
-        s[i] = sis;
+        mul(a[i], x, pm, pl);
+        s[i] = p_carry + pl;
+        p_carry = pm + ((s[i] < pl) * 1);
     }
 
     while (s[ssize - 1] == 0 && ssize > 0) {
         ssize--;
     }
-}
-
-// a *= x
-void muleq(uint64_t* a, uint64_t& asize, const uint64_t x) {
-    constexpr size_t shift = (sizeof(uint64_t) * 4UL);
-    constexpr uint64_t    half  = static_cast<uint64_t>(0xffffffffffffffffUL >> shift);
-
-    uint64_t p_carry = 0; // carry for product
-
-    for (size_t i = 0; i < asize; i++) {
-        const uint64_t ai = a[i];
-        const uint64_t xm = x >> shift;   // most significant half of x
-        const uint64_t xl = x & half;     // least significant half of x
-        const uint64_t am = ai >> shift;  // most significant half of a[i]
-        const uint64_t al = ai & half;    // least significant half of a[i]
-        const uint64_t mm = xm * am;
-        const uint64_t ml = xm * al;
-        const uint64_t lm = xl * am;
-        const uint64_t ll = xl * al;
-        const uint64_t o1 = (ml & half) + (lm & half) + (ll >> shift);
-        const uint64_t pm = mm + (ml >> shift) + (lm >> shift) + (o1 >> shift); // most significant half of product
-        const uint64_t pl = (o1 << shift) + (ll & half); // least significant half of product
-        
-        const uint64_t ai_new = p_carry + pl;       // a[i] after product (to calculate p_carry)
-        p_carry = pm + ((ai_new < pl) * 1);
-        a[i] = ai_new;
-    }
-
-    if (p_carry != 0) {
-        a[asize] = p_carry;
-        asize++;
-    } 
 }
 
 int main(int argc, char* argv[]) {
@@ -138,12 +115,13 @@ int main(int argc, char* argv[]) {
     uint64_t x2 = 30;
     uint64_t x3 = 3;
 
+    uint64_t z = 0;
+
     for (uint64_t i = 1; i <= N; ++i) {
         saxpy(n1, n1size, n0, n0size, x3, n1, n1size);      // n1 = (n0 * x3) + n1 
 
         char y = '0';
-        while (true) {
-            // while (diffeq(n1, n1size, n2, n2size)) {
+        while (y <= '9') { // y = n1//n2; n1 %= n2; 
             // checking to make sure a >= b;
             if (n2size > n1size) {
                 break;
@@ -177,15 +155,11 @@ int main(int argc, char* argv[]) {
             y += 1;
         }
 
-        if (y > '9') {
-            std::cerr << "y > '9'" << std::endl;
-            return -1;
-        }
         std::cout << y;
 
-        muleq(n0, n0size, x0);  // n0 *= x0
-        muleq(n1, n1size, x1);  // n1 *= x1
-        muleq(n2, n2size, x2);  // n2 *= x2
+        saxpy(n0, n0size, n0, n0size, x0, nullptr, z);  // n0 *= x0
+        saxpy(n1, n1size, n1, n1size, x1, nullptr, z);  // n1 *= x1
+        saxpy(n2, n2size, n2, n2size, x2, nullptr, z);  // n2 *= x2
 
         x0 += (20 * i) + 5;
         x1 += 270 * (i + 1);
